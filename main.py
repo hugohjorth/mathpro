a11ceaf24736a6aedb292bc32e01b808c9178715
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2020-03-20 08:30:48 +0100

    added comments
    added comments

--- main.py
+++ main.py
@@ -5,39 +5,32 @@ from google.cloud import vision
 
 import cv2
 
-from sympy import *
+from sympy import *   #Importerar Bibliotek
 
-x, y, z = symbols('x y z')
+x, y, z = symbols('x y z')  #Definerar vilka bokstäver som kan användas som variabler
 count = ['+', '-', '=']
 
-def removing_noise(imagePath):
+def removing_noise(imagePath):  #Funktion som gray scalar och blura bilden för att få bättre resultat
     img = cv2.imread(imagePath)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5),0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #Gör bilden grå
+    blur = cv2.GaussianBlur(gray, (5,5),0)  #Blurar den
     cv2.imwrite('./images/new.jpg',blur)
 
-def pic_to_text(infile):
-    """Detects text in an image file
+def pic_to_text(infile): #Funktion som tar in en bild och returnar texten som står i bilden
 
-    ARGS
-    infile: path to image file
-
-    RETURNS
-    String of text detected in image
-    """
-
-    # Instantiates a client
+    # skapar en client
     client = vision.ImageAnnotatorClient()
 
-    # Opens the input image file
+    # Öppnar bilden vi väljer som argument till funktionen
     with io.open(infile, 'rb') as image_file:
         content = image_file.read()
-
+  
     image = vision.types.Image(content=content)
 
-    # For dense text, use document_text_detection
-    # For less dense text, use text_detection
+
+    #Använder ocr som text detenction och sparar det i en variabel
     response = client.document_text_detection(image=image)
+    #Sparar bara texten vi får tillbaka i en separat variabel
     text = response.full_text_annotation.text
 
     return text
@@ -45,16 +38,19 @@ def pic_to_text(infile):
 removing_noise('./images/hugo.jpg')
 
 results = pic_to_text('./images/new.jpg')
-print(results)
 
 # for i in results:
 #     if not i.isdigit():
 #         if not i in count:
 #             pass
 
+#Funktion för att lösa  en ekvation
 def cal(thing, variable):
+    #skapar ekvationen men replacar "=" tecken med ett "," eftersom biblioteket använder kommat som ett likamed tecken
     main = sympify("Eq(" + thing.replace("=", ",")+")")
+    #löser ekvationen men min valda variabel
     answer = solveset(main, variable)
     return answer
 
+#Printar rätt svar
 print(cal(results.lower(),x))
